047957a46f33d65c772e1cc354c2c7022e1e4dbb
Em Karisch <emii.khaos@gmail.com>
2026-07-08 10:21:00 +0200

    feat: put borehole pump in proper subgroup

--- data-updates.lua
+++ data-updates.lua
@@ -3,3 +3,7 @@ if mods["SchallCircuitGroup"] then
   data.raw["item"]["factory-circuit-connector"].subgroup = "circuit-connection"
   data.raw["recipe"]["factory-circuit-connector"].subgroup = "circuit-connection"
 end
+
+-- Add borehole pump to proper production subgroup.
+data.raw["item"]["borehole-pump"].subgroup = "extraction-machine"
+data.raw["recipe"]["borehole-pump"].subgroup = "extraction-machine"
