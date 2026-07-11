f8af49df48534a4d3584e5d88ade556191996e82
Em Karisch <emii.khaos@gmail.com>
2026-07-11 06:47:06 +0200

    refactor: improve khaoslib usage

--- data-updates.lua
+++ data-updates.lua
@@ -1,9 +1,12 @@
+local khaoslib_item = require("__khaoslib__.prototypes.item")
+local khaoslib_recipe = require("__khaoslib__.prototypes.recipe")
+
 -- Add circuit connector to proper circuit subgroup.
 if mods["SchallCircuitGroup"] then
-  data.raw["item"]["factory-circuit-connector"].subgroup = "circuit-connection"
-  data.raw["recipe"]["factory-circuit-connector"].subgroup = "circuit-connection"
+  khaoslib_item:load("factory-circuit-connector"):set {subgroup = "circuit-connection"} :commit()
+  khaoslib_recipe:load("factory-circuit-connector"):set {subgroup = "circuit-connection"} :commit()
 end
 
 -- Add borehole pump to proper production subgroup.
-data.raw["item"]["borehole-pump"].subgroup = "extraction-machine"
-data.raw["recipe"]["borehole-pump"].subgroup = "extraction-machine"
+  khaoslib_item:load("borehole-pump"):set {subgroup = "extraction-machine"} :commit()
+  khaoslib_recipe:load("borehole-pump"):set {subgroup = "extraction-machine"} :commit()
